4f35b2ad5904f1bf61f2edb8f5f2c7f45a219331
ILLUMINATOR1337 <danil_zagirov@mail.ru>
2020-10-27 18:11:00 +0500

    added footer and maps

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,14 @@
 $(function(){
 
+   $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+      $(this)
+        .addClass('active').siblings().removeClass('active')
+        .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+    });
+
+    $('.slider').slick({
+      prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/arrow-left.svg"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="images/icons/arrow-right.svg"></button>'
+    });
+
 });
